9a726b9bf80083e7b9e7dac22951d2e5af1bf62f
zack <hi@zoeys.computer>
2024-11-25 16:17:10 -0500

    see what this changes hehe

--- twilight-http/src/client/builder.rs
+++ twilight-http/src/client/builder.rs
@@ -141,7 +141,9 @@ impl ClientBuilder {
         // Make sure it is either a bot or bearer token, and assume it's a bot
         // token if no prefix is given
         if !is_bot && !is_bearer {
-            token.insert_str(0, "Bot ");
+            tracing::info!(
+                "Caught a token that wasn't a bot or bearer token, assuming it's a bot token"
+            );
         }
 
         self.token.replace(Token::new(token.into_boxed_str()));
