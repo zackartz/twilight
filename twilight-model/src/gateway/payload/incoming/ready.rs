7583563b666cce5005fa351aa593693e8e56c29f
zack <hi@zoeys.computer>
2024-11-25 19:05:47 -0500

    stuff and things

--- twilight-model/src/gateway/payload/incoming/ready.rs
+++ twilight-model/src/gateway/payload/incoming/ready.rs
@@ -7,96 +7,400 @@ use crate::{
 };
 use serde::{Deserialize, Serialize};
 
-/// Session type for the connection
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-#[serde(rename_all = "snake_case")]
-pub enum SessionType {
-    /// Normal session
-    Normal,
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Ready {
+    pub t: String,
+    pub s: i64,
+    pub op: i64,
+    pub d: D,
 }
 
-/// Client information about the session
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct ClientInfo {
-    /// The client's version
-    pub version: u32,
-    /// The client's operating system
-    pub os: String,
-    /// The client type (e.g., "web")
-    pub client: String,
-}
-
-/// Information about a session
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct Session {
-    /// Session ID
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct D {
+    pub session_type: String,
+    pub api_code_version: i64,
+    pub presences: Vec<DPresence>,
+    pub notification_settings: NotificationSettings,
+    pub private_channels: Vec<PrivateChannel>,
+    pub consents: Consents,
+    pub guild_experiments: Vec<Vec<Option<DGuildExperiment>>>,
+    pub user_guild_settings: Vec<UserGuildSetting>,
+    pub user_settings_proto: String,
+    pub static_client_session_id: String,
+    pub guilds: Vec<Guild>,
+    pub explicit_content_scan_version: i64,
+    pub analytics_token: String,
     pub session_id: String,
-    /// Session status (e.g., "online", "dnd")
+    pub resume_gateway_url: String,
+    pub experiments: Vec<Vec<i64>>,
+    pub user: DUser,
+    pub v: i64,
+    pub country_code: String,
+    pub friend_suggestion_count: i64,
+    pub read_state: Vec<ReadState>,
+    pub auth_session_id_hash: String,
+    pub user_settings: UserSettings,
+    pub sessions: Vec<Session>,
+    pub notes: Notes,
+    pub geo_ordered_rtc_regions: Vec<String>,
+    pub relationships: Vec<Relationship>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Consents {
+    pub personalization: Personalization,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Personalization {
+    pub consented: bool,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum DGuildExperiment {
+    Integer(i64),
+    String(String),
+    UnionArray(Vec<TentacledGuildExperiment>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum TentacledGuildExperiment {
+    FluffyGuildExperiment(FluffyGuildExperiment),
+    UnionArray(Vec<StickyGuildExperiment>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum StickyGuildExperiment {
+    Integer(i64),
+    UnionArray(Vec<IndigoGuildExperiment>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum IndigoGuildExperiment {
+    PurpleGuildExperiment(PurpleGuildExperiment),
+    UnionArray(Vec<Option<IndecentGuildExperiment>>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum IndecentGuildExperiment {
+    Bool(bool),
+    Integer(i64),
+    UnionArray(Vec<HilariousGuildExperiment>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum HilariousGuildExperiment {
+    Integer(i64),
+    String(String),
+    UnionArray(Vec<AmbitiousGuildExperiment>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum AmbitiousGuildExperiment {
+    PurpleGuildExperiment(PurpleGuildExperiment),
+    UnionArray(Vec<CunningGuildExperiment>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum CunningGuildExperiment {
+    Integer(i64),
+    StringArray(Vec<String>),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct PurpleGuildExperiment {
+    pub s: i64,
+    pub e: i64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct FluffyGuildExperiment {
+    pub k: Vec<String>,
+    pub b: i64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Notes {}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Channel {
+    pub version: i64,
+    #[serde(rename = "type")]
+    pub channel_type: i64,
+    pub topic: Option<String>,
+    pub rate_limit_per_user: Option<i64>,
+    pub position: i64,
+    pub permission_overwrites: Vec<PermissionOverwrite>,
+    pub parent_id: Option<String>,
+    pub nsfw: Option<bool>,
+    pub name: String,
+    pub last_message_id: Option<String>,
+    pub id: String,
+    pub icon_emoji: Option<IconEmoji>,
+    pub flags: i64,
+    pub user_limit: Option<i64>,
+    pub bitrate: Option<i64>,
+    pub last_pin_timestamp: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct IconEmoji {
+    pub name: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct PermissionOverwrite {
+    #[serde(rename = "type")]
+    pub permission_overwrite_type: i64,
+    pub id: String,
+    pub deny: String,
+    pub allow: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Emoji {
+    pub version: i64,
+    pub require_colons: bool,
+    pub name: String,
+    pub managed: bool,
+    pub id: String,
+    pub available: bool,
+    pub animated: bool,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Member {
+    pub user: UserElement,
+    pub roles: Vec<String>,
+    pub pending: bool,
+    pub nick: Option<String>,
+    pub mute: bool,
+    pub joined_at: String,
+    pub flags: i64,
+    pub deaf: bool,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct UserElement {
+    pub username: String,
+    pub public_flags: i64,
+    pub id: String,
+    pub global_name: Option<String>,
+    pub display_name: Option<String>,
+    pub discriminator: String,
+    pub bot: Option<bool>,
+    pub avatar: Option<String>,
+    pub system: Option<bool>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct GuildPresence {
+    pub user: PartyClass,
     pub status: String,
-    /// Client information
-    pub client_info: ClientInfo,
-    /// List of activities for the session
+    pub client_status: ClientStatus,
     pub activities: Vec<Activity>,
 }
 
-#[derive(Clone, Debug, Deserialize, PartialEq, Serialize)]
-pub struct Ready {
-    /// Current application info
-    pub application: PartialApplication,
-    /// Array of guild objects
-    pub guilds: Vec<Guild>,
-    /// Used for resuming connections
-    pub resume_gateway_url: String,
-    /// Session ID
-    pub session_id: String,
-    /// Gateway version
-    #[serde(rename = "v")]
-    pub version: u8,
-    /// The shard information associated with this session, if sent when identifying
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub shard: Option<ShardId>,
-    /// Current user
-    pub user: CurrentUser,
-    /// Contains id and flags
-    pub analytics_token: String,
-    /// Connected user settings
-    #[serde(skip)]
-    pub user_settings: Option<UserSettings>,
-    /// List of servers for RTC regions
-    #[serde(default)]
-    pub geo_ordered_rtc_regions: Vec<String>,
-    /// Session type
-    pub session_type: SessionType,
-    /// List of read states
-    #[serde(default)]
-    pub read_state: Vec<ReadState>,
-    /// List of user sessions
-    #[serde(default)]
-    pub sessions: Vec<Session>,
-    /// Auth session ID hash
-    pub auth_session_id_hash: String,
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Assets {
+    pub large_text: String,
+    pub large_image: String,
 }
 
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct UserSettings;
-
-/// Read state for a channel
-#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
-pub struct ReadState {
-    /// ID of the channel
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct PartyClass {
     pub id: String,
-    /// Last message ID that was read
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Timestamps {
+    pub start: i64,
+    pub end: i64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ClientStatus {
+    pub web: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Role {
+    pub version: i64,
+    pub tags: Tags,
+    pub position: i64,
+    pub permissions: String,
+    pub name: String,
+    pub mentionable: bool,
+    pub managed: bool,
+    pub id: String,
+    pub hoist: bool,
+    pub flags: i64,
+    pub color: i64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Tags {
+    pub bot_id: Option<String>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct NotificationSettings {
+    pub flags: i64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct DPresence {
+    pub user: UserElement,
+    pub status: String,
+    pub client_status: ClientStatus,
+    pub activities: Vec<Activity>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct PrivateChannel {
+    #[serde(rename = "type")]
+    pub private_channel_type: i64,
+    pub recipients: Vec<UserElement>,
+    pub recipient_flags: i64,
     pub last_message_id: String,
-    /// Timestamp of the last pin
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub last_pin_timestamp: Option<Timestamp>,
-    /// Mention count
-    pub mention_count: u64,
-    /// Read state flags
-    pub flags: u64,
-    /// Last viewed timestamp
-    #[serde(skip_serializing_if = "Option::is_none")]
-    pub last_viewed: Option<u64>,
+    pub is_message_request: bool,
+    pub id: String,
+    pub flags: i64,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ReadState {
+    pub mention_count: i64,
+    pub last_pin_timestamp: String,
+    pub last_message_id: LastMessageId,
+    pub id: String,
+    pub flags: i64,
+    pub last_viewed: Option<i64>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum LastMessageId {
+    Integer(i64),
+    String(String),
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Relationship {
+    pub user_ignored: bool,
+    pub user: UserElement,
+    #[serde(rename = "type")]
+    pub relationship_type: i64,
+    pub since: Option<String>,
+    pub is_spam_request: bool,
+    pub id: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Session {
+    pub status: String,
+    pub session_id: String,
+    pub client_info: ClientInfo,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ClientInfo {
+    pub version: i64,
+    pub os: String,
+    pub client: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct DUser {
+    pub verified: bool,
+    pub username: String,
+    pub purchased_flags: i64,
+    pub pronouns: String,
+    pub premium_type: i64,
+    pub premium: bool,
+    pub mobile: bool,
+    pub mfa_enabled: bool,
+    pub id: String,
+    pub global_name: String,
+    pub flags: i64,
+    pub email: String,
+    pub discriminator: String,
+    pub desktop: bool,
+    pub bio: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct UserGuildSetting {
+    pub version: i64,
+    pub suppress_roles: bool,
+    pub suppress_everyone: bool,
+    pub notify_highlights: i64,
+    pub muted: bool,
+    pub mute_scheduled_events: bool,
+    pub mobile_push: bool,
+    pub message_notifications: i64,
+    pub hide_muted_channels: bool,
+    pub guild_id: String,
+    pub flags: i64,
+    pub channel_overrides: Vec<ChannelOverride>,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ChannelOverride {
+    pub muted: bool,
+    pub message_notifications: i64,
+    pub flags: i64,
+    pub collapsed: bool,
+    pub channel_id: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct UserSettings {
+    pub detect_platform_accounts: bool,
+    pub animate_stickers: i64,
+    pub inline_attachment_media: bool,
+    pub status: String,
+    pub message_display_compact: bool,
+    pub allow_activity_party_privacy_voice_channel: bool,
+    pub view_nsfw_guilds: bool,
+    pub timezone_offset: i64,
+    pub enable_tts_command: bool,
+    pub disable_games_tab: bool,
+    pub stream_notifications_enabled: bool,
+    pub animate_emoji: bool,
+    pub friend_source_flags: FriendSourceFlags,
+    pub allow_activity_party_privacy_friends: bool,
+    pub convert_emoticons: bool,
+    pub afk_timeout: i64,
+    pub passwordless: bool,
+    pub contact_sync_enabled: bool,
+    pub gif_auto_play: bool,
+    pub native_phone_integration_enabled: bool,
+    pub allow_accessibility_detection: bool,
+    pub friend_discovery_flags: i64,
+    pub show_current_game: bool,
+    pub developer_mode: bool,
+    pub view_nsfw_commands: bool,
+    pub render_reactions: bool,
+    pub locale: String,
+    pub render_embeds: bool,
+    pub inline_embed_media: bool,
+    pub default_guilds_restricted: bool,
+    pub explicit_content_filter: i64,
+    pub theme: String,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct FriendSourceFlags {
+    pub all: bool,
 }
 
 #[cfg(test)]
